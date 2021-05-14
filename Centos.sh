ffb3bcc4f97f5156b75ff78462beec8b84bd9323
S4nti148 <75098929+S4nti148@users.noreply.github.com>
2021-05-14 11:30:59 -0500

    Instalacion en Centos
    Validar versiones soportadas y usuario root

---
+++ Centos.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Se despliega a directorio /opt
+cd /opt
+#Se crea directorio llamado cylance 
+mkdir cylance
+#Se crea el archivo de config_defaults.txt
+cd cylance
+touch config_defaults.txt
+#Se comienza a inyectar la informacion requerida
+echo "Cual es el token?"
+read token 
+echo InstallToken=$token > config_defaults.txt
+echo SelfProtectionLevel=2 >> config_defaults.txt
+echo "Cual es el nombre de la zona?"
+read zona
+echo VenueZone=$zona >> config_defaults.txt
+echo LogLevel=2 >> config_defaults.txt
+echo "Procesando la informacion"
+echo " "
+echo "Comenzando con la instalacion del agente"
+echo "En que ruta esta el agente?"
+read ruta
+echo "-----------------------------------------------"
+ls $ruta   
+echo "-----------------------------------------------"
+cd $ruta
+echo "Cual es el nombre del archivo?"
+read nombre
+#dpkg -i $nombre  
+echo "Se esta instalando en $ruta, el nombre del archivo es $nombre"  
+ 
+#yum install $nombre 
+
